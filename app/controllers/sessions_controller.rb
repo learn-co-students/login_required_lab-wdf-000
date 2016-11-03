e50506fcc399c2a7fd957f70370d690c1edc5de0
jin501 <soljin01@gmail.com>
2016-11-03 17:21:29 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    redirect_to new_session_path unless session.include? :name
+    session[:name] = params[:name]
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to new_session_path
+  end
+
+
+
+end
